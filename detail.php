994a3807396d2628f1e505dd8bd01b112af0f7a3
markus.koost <markuskoost@gmail.com>
2020-05-13 20:50:42 +0300

    detail.php misc

--- detail.php
+++ detail.php
@@ -1,11 +1,11 @@
 <?php
 require 'connection.php';
 
-$image = $_POST['image'];
-$title = $_POST['title'];
-$description = $_POST['description'];
-$topic1 = $_POST['topic1'];
-$topic2 = $_POST['topic2'];
+$image = mysqli_real_escape_string($mysqli, $_POST['image']);
+$title = mysqli_real_escape_string($mysqli, $_POST['title']);
+$description = mysqli_real_escape_string($mysqli, $_POST['description']);
+$topic1 = mysqli_real_escape_string($mysqli, $_POST['topic1']);
+$topic2 = mysqli_real_escape_string($mysqli, $_POST['topic2']);
 
 if (isset($_POST['action'])) {
     echo "<img src='" . $image . "'";
